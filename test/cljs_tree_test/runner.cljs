75502ad205a3fae8abaeff2be16e2a1ec147ded8
David_Clark <clartaq@axaluna.com>
2019-03-03 15:50:16 -0500

    Added another test namespace.

--- test/cljs_tree_test/runner.cljs
+++ test/cljs_tree_test/runner.cljs
@@ -1,13 +1,12 @@
 (ns ^:figwheel-always cljs-tree-test.runner
   (:require [cljs.test :refer-macros [run-tests]]
+            [cljs-tree-test.tree-data-test]
             [cljs-tree-test.tree-id-test]
             [cljs-tree-test.vector-util-test]))
 
 (defn run-all-tests
   []
-  (run-tests 'cljs-tree-test.vector-util-test
-             'cljs-tree-test.tree-id-test))
+  (run-tests 'cljs-tree-test.tree-data-test
+             'cljs-tree-test.tree-id-test
+             'cljs-tree-test.vector-util-test))
 
-(defn on-js-reload
-  []
-  (println "reloaded test runner"))
