4fb47b3afc1c8b64a46dd110cc23d839c9690eb8
clartaq <clartaq@axaluna.com>
2019-07-12 12:42:52 -0400

    Moved the demo/test data hierarchy out of the core namespace.

---
+++ src/cljs_tree/demo_hierarchy.cljs
@@ -0,0 +1,109 @@
+(ns cljs-tree.demo-hierarchy
+  (:require  [reagent.core :as r]))
+
+;; The hierarchical tree of tags is contained in the following. It is possible
+;; to change it dynamically and have it re-render correctly.
+
+(defonce test-hierarchy
+         (r/atom {:title   "cljs-tree"
+                  :tagline "Some experiments with hierarchical data."
+                  :tree    [{:topic    "Journal"
+                             :expanded true
+                             :children [{:topic "2016"}
+                                        {:topic    "2017"
+                                         :expanded nil
+                                         :children [{:topic    "11 - November"
+                                                     :expanded true
+                                                     :children [{:topic "Christmas Shopping"}
+                                                                {:topic "Buy Groceries"}]}
+                                                    {:topic    "22 - November"
+                                                     :expanded true
+                                                     :children [{:topic "Bake Pies"}]}
+                                                    {:topic    "25 - November"
+                                                     :expanded true
+                                                     :children [{:topic "Cook Turkey"}]}]}
+                                        {:topic "2018"}]}
+
+                            {:topic    "Books"
+                             :expanded true
+                             :children [{:topic    "Favorite Authors"
+                                         :expanded true
+                                         :children [{:topic    "Gum-Lickin' Warburger"
+                                                     :expanded true
+                                                     :children [{:topic "Age"}
+                                                                {:topic "DOB"}
+                                                                {:topic "Obituary"}]}
+                                                    {:topic "Bob Martin"}]}
+                                        {:topic    "Genre"
+                                         :expanded true
+                                         :children [{:topic    "Science"
+                                                     :expanded nil
+                                                     :children [{:topic    "Astrophysics for People in a Hurry"
+                                                                 :expanded true
+                                                                 :children [{:topic    "Author"
+                                                                             :expanded true
+                                                                             :children [{:topic "Neil de Grasse Tyson"}]}
+                                                                            {:topic    "ISBN"
+                                                                             :expanded true
+                                                                             :children [{:topic "978-0-393-60939-4"}]}]}]}
+                                                    {:topic    "Science Fiction"
+                                                     :expanded nil
+                                                     :children [{:topic "Space Opera"}
+                                                                {:topic "Military"}]}
+                                                    {:topic "Horror"}
+                                                    {:topic "Fantasy"}
+                                                    {:topic "Biography"}
+                                                    {:topic "History"}
+                                                    {:topic    "Programming"
+                                                     :expanded true
+                                                     :children [{:topic "On Lisp"}
+                                                                {:topic "Getting Clojure"}
+                                                                {:topic    "Clean Code"
+                                                                 :expanded nil
+                                                                 :children [{:topic    "Author"
+                                                                             :expanded true
+                                                                             :children [{:topic "Robert Martin"}]}
+                                                                            {:topic    "ISBN-10"
+                                                                             :expanded true
+                                                                             :children [{:topic "0-13-235088-2"}]}
+                                                                            {:topic    "ISBN-13"
+                                                                             :expanded true
+                                                                             :children [{:topic "978-0-13-235088-4"}]}]}]}]}]}
+
+                            {:topic    "Programming"
+                             :expanded true
+                             :children [{:topic    "Language"
+                                         :expanded true
+                                         :children [{:topic    "Java"
+                                                     :expanded true
+                                                     :children [{:topic "Snippets"}
+                                                                {:topic "Books"}
+                                                                {:topic "Blogs"}
+                                                                {:topic "Gui Development"}]}
+                                                    {:topic    "Clojure"
+                                                     :expanded true
+                                                     :children [{:topic "Snippets"}
+                                                                {:topic "Books"}
+                                                                {:topic "Numerics"}]}
+                                                    {:topic    "Lisp"
+                                                     :expanded nil
+                                                     :children [{:topic "History"}
+                                                                {:topic "Weenies"}
+                                                                {:topic "The All Powerful"}]}]}]}
+
+                            {:topic    "Animals"
+                             :expanded true
+                             :children [{:topic "Birds"}
+                                        {:topic    "Mammals"
+                                         :expanded nil
+                                         :children [{:topic "Elephant"}
+                                                    {:topic "Mouse"}]}
+                                        {:topic "Reptiles"}]}
+
+                            {:topic    "Plants"
+                             :expanded true
+                             :children [{:topic    "Flowers"
+                                         :expanded true
+                                         :children [{:topic "Rose"}
+                                                    {:topic "Tulip"}]}
+                                        {:topic "Trees"}]}]}))
